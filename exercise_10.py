ae0ee5455f5454648e5ce230b2dd755830faafd1
Paloma Ribeiro <paloma.bareli@gmail.com>
2018-06-30 12:02:29 -0300

    Second part 30/06/2018

--- exercise_10.py
+++ exercise_10.py
@@ -8,8 +8,17 @@ for name in names:
 else:
     print("Rebeca not found\n\n")
 
+print("Test print with my name:")
 print("paloma","ribeiro")
 print("paloma","ribeiro", sep=".")
 print("paloma","ribeiro", sep=".", end="\n\n")
+
+print("\nList of my friends: ")
+#list compreension
+print([name.title() for name in names])
+
+print("\nIs Paloma in the list? ")
+#if ternário
+print("yes" if names.__contains__("paloma") else "no")
 
 
